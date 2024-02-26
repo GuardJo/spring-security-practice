3173e4b25609b7f176c0fc5706e3874498f49190
kyeongho <dagh1218@naver.com>
2024-02-27 01:14:06 +0900

    #5 UserDetailsService를 JdbcDetailsManager로 구현
    - h2 DB를 활용하여 JdbcDetailsManager를 일부 재설정해서 연동
        - JdbcDetailsManager는 UserDetailsManager에 대해 spring security 에서 제공하는 기본 구현체 중 하나임

--- basic-security/src/main/java/com/guardjo/practice.spring.security/basicsecurity/config/SecurityConfig.java
+++ basic-security/src/main/java/com/guardjo/practice.spring.security/basicsecurity/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.guardjo.practice.spring.security.basicsecurity.config;
 
-import java.util.List;
+import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +11,8 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import com.guardjo.practice.spring.security.basicsecurity.model.DummyUser;
-import com.guardjo.practice.spring.security.basicsecurity.service.CustomInmemoryUserService;
 
 @Configuration
 public class SecurityConfig {
@@ -22,8 +20,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(registry -> {
 				registry.requestMatchers(PathRequest.toH2Console()).permitAll()
-					.anyRequest().authenticated()
-					.requestMatchers("/errors").permitAll();
+					.anyRequest().authenticated();
 			})
 			.csrf(AbstractHttpConfigurer::disable)
 			.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
@@ -32,10 +29,16 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public UserDetailsService userDetailsService() {
-		var user = new DummyUser();
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+		String userByUsernameQuery = "select u.username, u.password, u.enabled from dummy_user u where u.username = ?";
+		String authsByUserQuery = "select a.username, a.authority from dummy_authority a where a.username = ?";
 
-		return new CustomInmemoryUserService(List.of(user));
+		var userDetailsService = new JdbcUserDetailsManager(dataSource);
+
+		userDetailsService.setUsersByUsernameQuery(userByUsernameQuery);
+		userDetailsService.setAuthoritiesByUsernameQuery(authsByUserQuery);
+
+		return userDetailsService;
 	}
 
 	@Bean
--- basic-security/src/main/resources/data.sql
+++ basic-security/src/main/resources/data.sql
@@ -1,4 +1,8 @@
-insert into dummy_user(username, password, authority)
-VALUES ('test1', '1234', 'READ');
-insert into dummy_user(username, password, authority)
-values ('test2', '1234', 'WRITE');
+insert into dummy_user(username, password)
+VALUES ('test1', '1234');
+insert into dummy_user(username, password)
+values ('test2', '1234');
+insert into dummy_authority(username, authority)
+VALUES ('test1', 'READ');
+insert into dummy_authority(username, authority)
+VALUES ('test2', 'WRITE');
