011d9c100f3a263c7c95112d565d2e76d9520ab1
kyeongho <dagh1218@naver.com>
2024-03-03 15:59:32 +0900

    #9 비동기 메소드 내 인증 객체 조회 구성
    - async를 통한 비동기 메소드 수행 구성
    - 별도 Thread에서 SecurityContext를 조회할 수 있도록 SecurityContextHolder Strategy 지정

--- basic-security/src/main/java/com/guardjo/practice.spring.security/basicsecurity/controller/BasicController.java
+++ basic-security/src/main/java/com/guardjo/practice.spring.security/basicsecurity/controller/BasicController.java
@@ -1,10 +1,16 @@
 package com.guardjo.practice.spring.security.basicsecurity.controller;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class BasicController {
 	@GetMapping("/status")
 	public String status() {
@@ -15,5 +21,13 @@ public class BasicController {
 	public String me(Authentication authentication) {
 
 		return (String)authentication.getPrincipal();
+	}
+
+	@Async
+	@GetMapping("/async-me")
+	public void async() {
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		Authentication authentication = securityContext.getAuthentication();
+		log.info("Thread : {}, me : {}", Thread.currentThread().getName(), authentication.getPrincipal());
 	}
 }
