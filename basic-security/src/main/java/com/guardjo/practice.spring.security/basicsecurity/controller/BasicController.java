6bae591b588e0bbca43924b2a05ce9dec594a729
kyeongho <dagh1218@naver.com>
2024-03-03 15:34:38 +0900

    #9 SecuritContext 내 저장된 현재 인증 객체 정보 반환
    - 인증 작업 후 SecurityContext에 담긴 Authentication 객체와 내부 필드 조회

--- basic-security/src/main/java/com/guardjo/practice.spring.security/basicsecurity/controller/BasicController.java
+++ basic-security/src/main/java/com/guardjo/practice.spring.security/basicsecurity/controller/BasicController.java
@@ -1,5 +1,6 @@
 package com.guardjo.practice.spring.security.basicsecurity.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,11 @@ public class BasicController {
 	@GetMapping("/status")
 	public String status() {
 		return "OK";
+	}
+
+	@GetMapping("/me")
+	public String me(Authentication authentication) {
+
+		return (String)authentication.getPrincipal();
 	}
 }
