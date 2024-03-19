468c666b0135ece115601ed7333302475a8a337d
Kyeongho <kyeongho_jo@lotte.net>
2024-03-19 17:07:38 +0900

    #13 엔드포인트별 권한 부여 설정
    - /auth 하위 엔드포인트에 대해 상황별 권한 부여
        - hasAuthoirty()를 통한 단일 권한 조회
        - hasAnyAuthority()를 통한 권한 조회
    - 인증 객체 조회 간 authority 도 주입하도록 개선
    - 테스트 데이터 추가

--- basic-security/src/main/java/com/guardjo/practice.spring.security/basicsecurity/config/CustomAuthenticationProvider.java
+++ basic-security/src/main/java/com/guardjo/practice.spring.security/basicsecurity/config/CustomAuthenticationProvider.java
@@ -1,7 +1,6 @@
 package com.guardjo.practice.spring.security.basicsecurity.config;
 
-import java.util.List;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,31 +11,29 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import lombok.RequiredArgsConstructor;
-
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationProvider implements AuthenticationProvider {
-	private final UserDetailsService userDetailsService;
-	private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
-	@Override
-	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		String username = authentication.getName();
-		String password = String.valueOf(authentication.getCredentials());
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
 
-		UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-		if (passwordEncoder.matches(password, userDetails.getPassword())) {
-			return new UsernamePasswordAuthenticationToken(username, password, List.of());
-		} else {
-			throw new AuthenticationCredentialsNotFoundException("Failed Authentication");
-		}
-	}
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(username, password, userDetails.getAuthorities());
+        } else {
+            throw new AuthenticationCredentialsNotFoundException("Failed Authentication");
+        }
+    }
 
-	@Override
-	public boolean supports(Class<?> authentication) {
-		return UsernamePasswordAuthenticationToken.class
-			.isAssignableFrom(authentication);
-	}
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class
+                .isAssignableFrom(authentication);
+    }
 }
--- basic-security/src/main/java/com/guardjo/practice.spring.security/basicsecurity/config/SecurityConfig.java
+++ basic-security/src/main/java/com/guardjo/practice.spring.security/basicsecurity/config/SecurityConfig.java
@@ -1,10 +1,9 @@
 package com.guardjo.practice.spring.security.basicsecurity.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
@@ -14,39 +13,45 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SecurityConfig {
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(registry -> {
-				registry.requestMatchers(PathRequest.toH2Console()).permitAll()
-					.anyRequest().authenticated();
-			})
-			.csrf(AbstractHttpConfigurer::disable)
-			.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-			.httpBasic(AbstractHttpConfigurer::disable)
-			.formLogin(custom -> {
-				custom.successHandler(new CustomSuccessHandler());
-				custom.failureHandler(new CustomFailureHandler());
-			});
-		return http.build();
-	}
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(registry -> {
+                    registry.requestMatchers(PathRequest.toH2Console()).permitAll()
+                            .requestMatchers(HttpMethod.GET, "/auth/**")
+                            .hasAnyAuthority("READ", "WRITE")
+                            .requestMatchers(HttpMethod.POST, "/auth/**")
+                            .hasAuthority("WRITE")
+                            .anyRequest().authenticated();
+                })
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(custom -> {
+                    custom.successHandler(new CustomSuccessHandler());
+                    custom.failureHandler(new CustomFailureHandler());
+                });
+        return http.build();
+    }
 
-	@Bean
-	public UserDetailsService userDetailsService(DataSource dataSource) {
-		String userByUsernameQuery = "select u.username, u.password, u.enabled from dummy_user u where u.username = ?";
-		String authsByUserQuery = "select a.username, a.authority from dummy_authority a where a.username = ?";
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String userByUsernameQuery = "select u.username, u.password, u.enabled from dummy_user u where u.username = ?";
+        String authsByUserQuery = "select a.username, a.authority from dummy_authority a where a.username = ?";
 
-		var userDetailsService = new JdbcUserDetailsManager(dataSource);
+        var userDetailsService = new JdbcUserDetailsManager(dataSource);
 
-		userDetailsService.setUsersByUsernameQuery(userByUsernameQuery);
-		userDetailsService.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        userDetailsService.setUsersByUsernameQuery(userByUsernameQuery);
+        userDetailsService.setAuthoritiesByUsernameQuery(authsByUserQuery);
 
-		return userDetailsService;
-	}
+        return userDetailsService;
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
